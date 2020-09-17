821aa475f94285003c3cdcf17872e550c4a9ce74
Sergey Zvyagintsev <nitron.ru@gmail.com>
2020-09-17 08:07:33 +0700

    implement WebRtcEndpoint bandwidth configuration methods

--- src/MgKurentoClient/WebRtcEndpoint.php
+++ src/MgKurentoClient/WebRtcEndpoint.php
@@ -43,4 +43,44 @@ class WebRtcEndpoint extends MediaElement implements Interfaces\WebRtcEndpoint
     {
         return $this->remoteInvoke('addIceCandidate', ['candidate' => $candidate]);
     }
+
+    public function setMaxVideoSendBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMaxVideoSendBandwidth ', ['maxVideoSendBandwidth' => $value]);
+    }
+
+    public function setMinVideoSendBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMinVideoSendBandwidth ', ['minVideoSendBandwidth' => $value]);
+    }
+
+    public function setMaxVideoRecvBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMaxVideoRecvBandwidth ', ['maxVideoRecvBandwidth' => $value]);
+    }
+
+    public function setMinVideoRecvBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMinVideoRecvBandwidth ', ['minVideoRecvBandwidth' => $value]);
+    }
+
+    public function setMaxAudioSendBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMaxAudioSendBandwidth ', ['maxAudioSendBandwidth' => $value]);
+    }
+
+    public function setMinAudioSendBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMinAudioSendBandwidth ', ['minAudioSendBandwidth' => $value]);
+    }
+
+    public function setMaxAudioRecvBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMaxAudioRecvBandwidth ', ['maxAudioRecvBandwidth' => $value]);
+    }
+
+    public function setMinAudioRecvBandwidth(int $value): Promise
+    {
+        return $this->remoteInvoke('setMinAudioRecvBandwidth ', ['minAudioRecvBandwidth' => $value]);
+    }
 }
